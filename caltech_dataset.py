4d7b2327e4e03d4ecc1b30ac8ef21bdd4906e492
elia migliore <migliorelia@gmail.com>
2019-12-02 12:58:50 +0100

    solved boolean expression that was incorrect

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -51,11 +51,12 @@ class Caltech(VisionDataset):
     def __init__(self, root, split='train', transform=None, target_transform=None):
         super(Caltech, self).__init__(root, transform=transform, target_transform=target_transform)
 
-        if not (split!='train' and split!='test'):
+        if split!='train' and split!='test':
             print("error: split must be or train or test")
             sys.exit(1)
 
         split = split + ".txt"
+        print(split)
 
         with open(split,'r') as f:
             line = f.readline()
